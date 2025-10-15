4e67e4b0f991bca6f14b0453be88fd28dee6a05d
Digital-Fingers-Team <baraasaad007@gmail.com>
2025-10-15 19:30:39 +0300

    11

---
+++ public/firebase.js
@@ -0,0 +1,27 @@
+// firebase.js
+
+// 1️⃣ Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
+
+// 2️⃣ Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCPLU5MPZbNqUEBauKPWn8ui4eZUSzZf8s",
+  authDomain: "dt-edu-1d5ad.firebaseapp.com",
+  projectId: "dt-edu-1d5ad",
+  storageBucket: "dt-edu-1d5ad.firebasestorage.app",
+  messagingSenderId: "116202003139",
+  appId: "1:116202003139:web:9b6378faf2adf1e942a076",
+  measurementId: "G-Q3TSMV8QL1"
+};
+
+// 3️⃣ Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// 4️⃣ Export for use in other files
+export { auth, db, storage, signInWithEmailAndPassword, onAuthStateChanged, signOut, collection, addDoc, getDocs, query, where, ref, uploadBytes, getDownloadURL };
